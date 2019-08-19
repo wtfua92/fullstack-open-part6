19f295bbc6bd5e04ea0f27bf9c62a6698e83da6c
Andrii Tynok <atynok@homeaway.com>
2019-08-19 14:18:11 +0300

    6.14 6.15

--- redux-anecdotes-master/src/components/AnecdoteList.js
+++ redux-anecdotes-master/src/components/AnecdoteList.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Filter from "./Filter";
+import {voteAction} from "../reducers/anecdoteReducer";
 
-function AnecdoteList({filteredAnecdotes}) {
+function AnecdoteList({anecdotesToShow, voteAction}) {
     return (
         <div>
             <Filter />
             <h2>Anecdotes</h2>
             {
-                filteredAnecdotes
+                anecdotesToShow
                     .map(anecdote =>
                         <div key={anecdote.id}>
                             <div>
                                 {anecdote.content}
+                                <br/>
+                                has {anecdote.votes} <button type="button" onClick={() => voteAction(anecdote.id)}>vote</button>
                             </div>
                         </div>
                     )
@@ -23,8 +26,12 @@ function AnecdoteList({filteredAnecdotes}) {
 
 const mapStateToProps = ({anecdotes, filter}) => {
     return {
-        filteredAnecdotes: anecdotes.anecdotes.filter((a) => a.content.includes(filter))
+        anecdotesToShow: anecdotes.anecdotes.filter((a) => a.content.includes(filter)).sort((a, b) => b.votes - a.votes)
     };
 };
 
-export default connect(mapStateToProps)(AnecdoteList);
+const mapDispatchToProps = {
+    voteAction
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnecdoteList);
